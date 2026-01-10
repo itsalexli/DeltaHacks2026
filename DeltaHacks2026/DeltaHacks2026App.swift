8ea6f76e1109df06b82fafc646f8ec9a53f3606c
itsalexli <alex.liyou@gmail.com>
2026-01-10 15:19:45 -0500

    Initial Commit

---
+++ DeltaHacks2026/DeltaHacks2026App.swift
@@ -0,0 +1,17 @@
+//
+//  DeltaHacks2026App.swift
+//  DeltaHacks2026
+//
+//  Created by Alexander Li on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct DeltaHacks2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
